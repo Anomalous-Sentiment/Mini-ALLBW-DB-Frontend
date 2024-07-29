8b7b2087e9c8098e38444f40c13c7188ce4b5998
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-07-29 14:52:11 +0800

    Updated memoria api to include skill magnification values

--- prisma/views/public/gvg_support_magnification.sql
+++ prisma/views/public/gvg_support_magnification.sql
@@ -1,7 +1,5 @@
 SELECT
-  mem.unique_id,
   gvg_sk.skill_mst_id,
-  gvg_sk.attack_magnification,
   gvg_sk.buffer_magical_attack_magnification,
   gvg_sk.buffer_magical_defense_magnification,
   gvg_sk.buffer_physical_attack_magnification,
@@ -10,14 +8,9 @@ SELECT
   gvg_sk.debuffer_magical_defense_magnification,
   gvg_sk.debuffer_physical_attack_magnification,
   gvg_sk.debuffer_physical_defense_magnification,
-  gvg_sk.recovery_magnification,
+  gvg_sk.attack_up_magnification,
   gvg_sk.buffer_up_magnification,
+  gvg_sk.recovery_up_magnification,
   gvg_sk.use_sp_reduce_magnification
 FROM
-  (
-    (
-      skills gvg_sk
-      JOIN memoria mem ON ((mem.gvg_skill_mst_id = gvg_sk.skill_mst_id))
-    )
-    JOIN unique_memoria u_mem ON ((u_mem.unique_id = mem.unique_id))
-  );
+  skills gvg_sk;
