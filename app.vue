804163d04cc0134061a42cb5f3dc3bc379747bd3
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-07-31 00:40:10 +0800

    Moved ag table restyling to app.vue for global effect

--- app.vue
+++ app.vue
@@ -3,3 +3,28 @@
       <NuxtPage />
     </NuxtLayout>
   </template>
+
+<style>
+/* Making ag grid inherit colours from the primevue theme preset */
+.ag-theme-quartz-dark {
+    --ag-background-color: var(--p-content-background);
+    --ag-header-background-color: var(--p-content-background);
+    /* var(--p-surface-950) */
+    --ag-foreground-color: var(--p-text-color);
+    --ag-border-color: var(--p-datatable-header-cell-border-color);
+    /* --ag-border-color: var(--p-surface-800);
+    --ag-border-color: var(--p-surface-200); */
+    --ag-row-height: '50%';
+}
+
+.centered-img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.centered-cell {
+    display: flex !important;
+    align-items: center;
+}
+</style>
