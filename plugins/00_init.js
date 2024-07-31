9fe9ef74c8290e760f9d413ac5ba65f03672cd67
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-07-31 14:48:07 +0800

    Removed code toggling dark mode on clientside as it is now done on serverside

--- plugins/00_init.js
+++ plugins/00_init.js
@@ -5,8 +5,9 @@ function toggleColorScheme() {
 
 export default defineNuxtPlugin((nuxtApp) => {
     // Set to app to dark mode by default
-    nuxtApp.hook('app:mounted', () => {
-        toggleColorScheme()
+    nuxtApp.hook('app:beforeMount', () => {
+        // Class is currently set to dark mode on server side. If not server side rendered, then un-comment this to apply on client side
+        // toggleColorScheme()
     })
     // Provide a function to toggle app colour scheme
     nuxtApp.provide('toggleColorScheme', toggleColorScheme)
