aa5870f870c1a938d003c76dd50d4a71104e71d1
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-07-31 13:50:47 +0800

    Removed primevue plugin as it is no longer needed due to primevue module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,57 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from '@primevue/themes/aura';
+import { definePreset } from '@primevue/themes';
 
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{slate.50}',
+            100: '{slate.100}',
+            200: '{slate.200}',
+            300: '{slate.300}',
+            400: '{slate.400}',
+            500: '{slate.500}',
+            600: '{slate.600}',
+            700: '{slate.700}',
+            800: '{slate.800}',
+            900: '{slate.900}',
+            950: '{slate.950}'
+        },
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '{slate.50}',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.900}',
+                    950: '{slate.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.900}',
+                    950: '{slate.950}'
+                }
+            }
+    },
+}});
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
@@ -13,8 +64,11 @@ export default defineNuxtConfig({
   primevue: {
     options: {
         theme: {
-            preset: Aura
-        }
+            preset: MyPreset,
+            options: {
+              darkModeSelector: '.app-dark-mode',
+            }
+        },
     }
   },
   components: {
