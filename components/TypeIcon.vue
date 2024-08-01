9ed6a6464e22ed77c68c140a538ac373a3f13f68
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-08-02 00:40:15 +0800

    Changed icon naming convention

--- components/TypeIcon.vue
+++ components/TypeIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <img :src="`/img/icons/role_${props.params.value}.png`" alt="Role Icon" width="32px" height="32px" :style="{filter: 'invert(' + props.params.schemeVal + ')'}" loading="lazy">
+    <img :src="`/img/icons/card_type_${props.params.value}.png`" alt="Role Icon" width="32px" height="32px" :style="{filter: 'invert(' + props.params.schemeVal + ')'}" loading="lazy">
 </template>
 
 <script setup>
