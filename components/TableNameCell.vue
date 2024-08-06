e5dd0a9dc295cde6df493b89618063b5daeefb28
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2024-08-06 14:14:02 +0800

    Added text to signify awakened memoria in memoria table

--- components/TableNameCell.vue
+++ components/TableNameCell.vue
@@ -1,9 +1,13 @@
 <template>
-
-        <NuxtLink :to="`/card/${props.params.data['unique_id']}`">
+<div>
+    <NuxtLink :to="`/card/${props.params.data['unique_id']}`">
         {{ props.params.value }}
+        {{ ' ' }}
     </NuxtLink>
-
+    <span style="color: red;">
+            {{ props.params.data['super_awakened'] ? '(Super Awakened)' :  props.params.data['awakened'] ? '(Awakened)' :  ''}}
+    </span>
+</div>
 
 </template>
 
